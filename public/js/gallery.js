571a7ec07269549ec73e63bd79b226c970cc44d2
FernandoDAMoreira <fernandomoreira1996@gmail.com>
2023-04-29 13:43:56 +0100

    Updated some stylings and animations

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -8,13 +8,13 @@ $(document).ready(function () {
 
         if (value == "all") {
             // $('.filter').removeClass('hidden');
-            $('.filter').show('1000');
+            $('.filter').show();
         }
         else {
             // $('.filter[filter-item="'+value+'"]').removeClass('hidden');
             // $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.' + value).hide('3000');
-            $('.filter').filter('.' + value).show('3000');
+            $(".filter").not('.' + value).hide();
+            $('.filter').filter('.' + value).show();
 
         }
     });
